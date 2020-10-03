756a01ab228ce078a810ad17c42932b5060fc70a
Sathish Balaji <sathishbalajissb@gmail.com>
2020-10-04 04:24:09 +0530

    Initial

---
+++ src/main/java/com/example/customerdbmigration/CustomerdbmigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerdbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdbmigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdbmigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customerdbmigration/CustomerdbmigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerdbmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerdbmigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
