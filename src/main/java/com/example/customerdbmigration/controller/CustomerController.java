22d4efe347e8d2391fe22a0cca4d527074c74b2f
Sathish Balaji <sathishbalajissb@gmail.com>
2020-10-04 08:35:48 +0530

    Handle CORS for local Angular development

--- src/main/java/com/example/customerdbmigration/controller/CustomerController.java
+++ src/main/java/com/example/customerdbmigration/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.customerdbmigration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.example.customerdbmigration.model.Customer;
 import com.example.customerdbmigration.service.impl.CustomerServiceImpl;
 
 @RestController
+@CrossOrigin
 public class CustomerController {
 
     @Autowired
